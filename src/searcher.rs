f839433e613591e8477320d0535f5002e6bee83a
Franklin Delehelle <franklin.delehelle@irit.fr>
2019-01-15 19:15:31 +0100

    Add SIMD support.
    Unfortunately slower than standard string comparison, so fall back to
    std::Ordering::cmp() by default.

--- src/searcher.rs
+++ src/searcher.rs
@@ -13,6 +13,83 @@ pub struct Searcher {
 }
 
 static CACHE_LEN: usize = 8;
+const SSE_STRIDE: usize = 16;
+const AVX_STRIDE: usize = 32;
+
+/// Like above but with 32 byte slices
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "avx2")]
+pub unsafe fn avx_compare_mask(one: &[u8], two: &[u8]) -> i32 {
+    use std::arch::x86_64::*;
+    let onev = _mm256_loadu_si256(one.as_ptr() as *const _);
+    let twov = _mm256_loadu_si256(two.as_ptr() as *const _);
+    let mask = _mm256_cmpeq_epi8(onev, twov);
+    !_mm256_movemask_epi8(mask)
+}
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "sse4.2")]
+pub unsafe fn sse_compare_mask(one: &[u8], two: &[u8]) -> i32 {
+    use std::arch::x86_64::*;
+
+    // too lazy to figure out the bit-fiddly way to get this mask
+    const HIGH_HALF_MASK: u32 = 0b11111111111111110000000000000000;
+
+    debug_assert!(is_x86_feature_detected!("sse4.2"));
+
+    let onev = _mm_loadu_si128(one.as_ptr() as *const _);
+    let twov = _mm_loadu_si128(two.as_ptr() as *const _);
+    let mask = _mm_cmpeq_epi8(onev, twov);
+    (!_mm_movemask_epi8(mask)) ^ HIGH_HALF_MASK as i32
+}
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "avx2")]
+pub unsafe fn ne_idx_avx(one: &[u8], two: &[u8]) -> std::cmp::Ordering {
+    let min_len = one.len().min(two.len());
+    let mut idx = 0;
+    while idx < min_len {
+        let stride_len = AVX_STRIDE.min(min_len - idx);
+        let mask = avx_compare_mask(
+            &one.get_unchecked(idx..idx + stride_len),
+            &two.get_unchecked(idx..idx + stride_len),
+        );
+        // at the end of the slice the mask might include garbage bytes, so
+        // we ignore matches that are OOB
+        if mask != 0 && idx + (mask.trailing_zeros() as usize) < min_len {
+            let i = idx + mask.trailing_zeros() as usize;
+            return one[i].cmp(&two[i]);
+            // return Some(idx + mask.trailing_zeros() as usize);
+        }
+        idx += AVX_STRIDE;
+    }
+    std::cmp::Ordering::Equal
+}
+#[doc(hidden)]
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "sse4.2")]
+pub unsafe fn ne_idx_sse(one: &[u8], two: &[u8]) -> std::cmp::Ordering {
+    let min_len = one.len().min(two.len());
+    let mut idx = 0;
+    while idx < min_len {
+        let stride_len = SSE_STRIDE.min(min_len - idx);
+        let mask = sse_compare_mask(
+            &one.get_unchecked(idx..idx + stride_len),
+            &two.get_unchecked(idx..idx + stride_len),
+        );
+        if mask != 0 && idx + (mask.trailing_zeros() as usize) < min_len {
+            let i = idx + mask.trailing_zeros() as usize;
+            return one[i].cmp(&two[i]);
+            // return Some(idx + mask.trailing_zeros() as usize);
+        }
+        idx += SSE_STRIDE;
+    }
+    std::cmp::Ordering::Equal
+}
+#[inline]
+#[allow(dead_code)]
+#[doc(hidden)]
+pub fn ne_idx_fallback(one: &[u8], two: &[u8]) -> Option<usize> {
+    one.iter().zip(two.iter()).position(|(a, b)| a != b)
+}
 
 impl Searcher {
     fn indexize(p: &[u8]) -> u64 {
@@ -79,9 +156,13 @@ impl Searcher {
 
 
     pub fn search(&self, dna: &[u8], sa: &[idx], pattern: &[u8]) -> Vec<Segment> {
+        #[inline]
         fn stringcmp(a: &[u8], b: &[u8]) -> std::cmp::Ordering {
-            return a.cmp(b)
+            // return unsafe { ne_idx_sse(a, b) };
+            // return unsafe { ne_idx_avx(a, b) };
+            return a.cmp(b);
         }
+
         let index = Searcher::indexize(pattern);
 
         if !self.cache.contains_key(&index) {
@@ -98,15 +179,14 @@ impl Searcher {
                 stringcmp(&dna[*x as usize .. *x as usize + pattern.len()], &pattern)
             });
 
-        let mut rr = Vec::with_capacity(range.end - range.start + 1);
-        for start in &sa[lstart + range.start .. lstart + range.end] {
-            let start = *start as usize;
-            rr.push(Segment {
-                tag: 0,
-                start: self.offset + start,
-                end: self.offset + start + pattern.len(),
-            });
-        }
-        rr
+        sa[lstart + range.start .. lstart + range.end]
+            .iter()
+            .map(|start|
+                 Segment {
+                     tag: 0,
+                     start: self.offset + *start as usize,
+                     end: self.offset + *start as usize + pattern.len(),
+                 })
+            .collect()
     }
 }

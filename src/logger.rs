02baa0c68e082d2de89b5f7795417357af3d5f5e
Franklin Delehelle <franklin.delehelle@irit.fr>
2019-03-04 18:52:11 +0100

    Logger does not output color codes when not in a TTY

--- src/logger.rs
+++ src/logger.rs
@@ -13,24 +13,45 @@ impl log::Log for Logger {
     }
 
     fn log(&self, record: &Record) {
-        println!("{}",
-                 match record.level() {
-                     Level::Error => {
-                         format!("{} {}", "✖".red(), record.args().to_string().red().bold())
-                     }
-                     Level::Warn => {
-                         format!("{} {}", "⚠".yellow(), record.args().to_string().yellow())
-                     }
-                     Level::Info => {
-                         format!("{}", record.args())
-                     }
-                     Level::Trace => {
-                         format!("{} {}", "▷".cyan(), record.args())
-                     }
-                     Level::Debug => {
-                         format!("{} {}", "❖".blue(), record.args().to_string().blue())
-                     }
-                 });
+        if isatty::stdout_isatty() {
+            println!("{}",
+                     match record.level() {
+                         Level::Error => {
+                             format!("{} {}", "✖".red(), record.args().to_string().red().bold())
+                         }
+                         Level::Warn => {
+                             format!("{} {}", "⚠".yellow(), record.args().to_string().yellow())
+                         }
+                         Level::Info => {
+                             format!("{}", record.args())
+                         }
+                         Level::Trace => {
+                             format!("{} {}", "▷".cyan(), record.args())
+                         }
+                         Level::Debug => {
+                             format!("{} {}", "❖".blue(), record.args().to_string().blue())
+                         }
+                     });
+        } else {
+            println!("{}",
+                     match record.level() {
+                         Level::Error => {
+                             format!("{} {}", "✖", record.args().to_string().red().bold())
+                         }
+                         Level::Warn => {
+                             format!("{} {}", "⚠", record.args().to_string().yellow())
+                         }
+                         Level::Info => {
+                             format!("{}", record.args())
+                         }
+                         Level::Trace => {
+                             format!("{} {}", "▷", record.args())
+                         }
+                         Level::Debug => {
+                             format!("{} {}", "❖", record.args().to_string().blue())
+                         }
+                     });
+        }
     }
 
     fn flush(&self) {}

59063020a8c94abc84000e54fc35b8676b057b60
Franklin Delehelle <franklin.delehelle@irit.fr>
2019-11-25 17:59:34 +0100

    Add a first version of asgart-extract

---
+++ src/bin/asgart-extract.rs
@@ -0,0 +1,167 @@
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate clap;
+extern crate asgart;
+
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+
+use asgart::log::LevelFilter;
+use asgart::logger::Logger;
+use clap::{App, AppSettings, Arg};
+
+use asgart::errors::*;
+use asgart::utils;
+
+
+fn read_fasta(filename: &str) -> Result<(Vec<u8>)> {
+    let mut r = Vec::new();
+
+    let reader = bio::io::fasta::Reader::from_file(filename)
+        .chain_err(|| format!("Unable to open `{}`", filename))?;
+
+    for record in reader.records() {
+        let record = record.chain_err(|| {
+            format!("Unable to read {:?}: not a FASTA file", Path::new(filename).file_name().unwrap())
+        })?;
+
+        r.extend_from_slice(record.seq());
+    }
+
+    Ok(r)
+}
+
+fn main() {
+    Logger::init(LevelFilter::Info).expect("Unable to initialize logger");
+    let x = run();
+    if let Err(ref e) = x {
+        error!("{}", e);
+        for e in e.iter().skip(1) {
+            println!("{}", e);
+        }
+        if let Some(backtrace) = e.backtrace() {
+            println!("Backtrace: {:?}", backtrace);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let args = App::new("ASGART extract")
+        .setting(AppSettings::ColoredHelp)
+        .setting(AppSettings::ColorAuto)
+        .setting(AppSettings::VersionlessSubcommands)
+        .setting(AppSettings::UnifiedHelpMessage)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("ASGART extract pulls out duplication families as described in an ASGART JSON file into a serie of FASTA files, one per family.")
+        .arg(Arg::with_name("INPUT")
+             .help("Set the input JSON file(s) to use")
+             .required(true)
+             .takes_value(true)
+             .min_values(1))
+        .arg(Arg::with_name("prefix")
+             .short("p")
+             .long("prefix")
+             .help("A prefix to append to the output files")
+             .takes_value(true)
+             .number_of_values(1))
+        .arg(Arg::with_name("locations")
+             .short("l")
+             .long("locations")
+             .help("Where to find the original FASTA files; might take multiple values")
+             .takes_value(true))
+        .arg(Arg::with_name("max-family-size")
+             .short("M")
+             .long("max-family-size")
+             .help("Skip families with more duplicons than specified")
+             .takes_value(true))
+        .arg(Arg::with_name("no-direct")
+             .short("d")
+             .long("no-direct")
+             .help("Skip direct families"))
+        .arg(Arg::with_name("no-reversed")
+             .short("r")
+             .long("no-reversed")
+             .help("Skip reversed families"))
+        .arg(Arg::with_name("no-uncomplemented")
+             .short("u")
+             .long("no-uncomplemented")
+             .help("Skip uncomplemented families"))
+        .arg(Arg::with_name("no-complemented")
+             .short("t")
+             .long("no-complemented")
+             .help("Skip complemented families"))
+        .get_matches();
+
+    let input = value_t!(args, "INPUT", String).unwrap();
+    let prefix = value_t!(args, "prefix", String).unwrap_or("".to_string());
+    let locations = values_t!(args, "locations", String).unwrap_or(vec!["".to_owned()]);
+    let max_family_len = value_t!(args, "max-family-size", usize).unwrap_or(100_000_000);
+    info!("Max. duplicons count: {}", max_family_len);
+
+    info!("Reading {}...", &input);
+    let result = asgart::structs::RunResult::from_files(&[input])?;
+    info!("Done.");
+
+    let strands_files = result.strand.name.split(",")
+        .map(str::trim)
+        .map(|name| {
+            for location in &locations {
+                let path = format!("{}/{}", location, name);
+                if Path::new(&path).exists() {
+                    return Ok(path)
+                }
+            }
+            Err(format!("Unable to find {} in the locations provided ({})", name, locations.join(", ")))
+        })
+        .collect::<std::result::Result<Vec<_>, _>>()
+        .unwrap_or_else(|msg| {error!("{}", msg); Vec::new()});
+
+    let mut strand = Vec::new();
+    for strand_file in &strands_files {
+        info!("Reading {}...", strand_file);
+        let new_strand = read_fasta(strand_file).chain_err(|| format!("Unable to parse `{}`", strand_file))?;
+        strand.extend(new_strand);
+        info!("Done.");
+    }
+
+    info!("Writing results...");
+    for (i, family) in result.families.iter().enumerate() {
+        if family.len() > max_family_len {continue}
+
+        let family_id = format!("family-{}", i);
+        let out_file_name = format!("{}{}.fa", prefix, family_id);
+        for (j, sd) in family.iter().enumerate() {
+            if args.is_present("no-direct") && !sd.reversed {continue};
+            if args.is_present("no-reversed") && sd.reversed {continue};
+            if args.is_present("no-uncomplemented") && !sd.complemented {continue};
+            if args.is_present("no-complemented") && sd.complemented {continue};
+
+            let left_seq = strand[sd.global_left_position .. sd.global_left_position + sd.left_length].to_vec();
+            let mut right_seq = strand[sd.global_right_position .. sd.global_right_position + sd.right_length].to_vec();
+            if sd.reversed {right_seq.reverse();}
+            if sd.complemented {right_seq = utils::complemented(&right_seq);}
+
+            let mut file = OpenOptions::new().append(true).create(true).open(&out_file_name)
+                .chain_err(|| format!("Unable to write in `{}`", out_file_name))?;
+            file.write_all(format!(">{} {}-{} duplicon-{}-1\n",
+                                   sd.chr_left, sd.chr_left_position, sd.chr_left_position+sd.left_length , j).as_bytes())
+                .chain_err(|| "Unable to write in file.")?;
+            file.write_all(&left_seq).chain_err(|| "Unable to write in file.")?;
+            file.write_all(b"\n").chain_err(|| "Unable to write in file.")?;
+            file.write_all(format!(">{} {}-{} duplicon-{}-2\n",
+                                   sd.chr_right, sd.chr_right_position, sd.chr_right_position+sd.right_length, j).as_bytes())
+                .chain_err(|| "Unable to write in file.")?;
+            file.write_all(&right_seq).chain_err(|| "Unable to write in file.")?;
+            file.write_all(b"\n").chain_err(|| "Unable to write in file.")?;
+        }
+    }
+    info!("Done.");
+
+
+    Ok(())
+}

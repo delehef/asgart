ae7c86064ebb628b7398610e2b93e81c500a26bf
Franklin Delehelle <franklin.delehelle@odena.eu>
2020-11-14 15:57:49 +0100

    [asgart-plot] Add the primitive backbone of a drawing system

--- src/plot/mod.rs
+++ src/plot/mod.rs
@@ -42,3 +42,68 @@ pub struct Feature {
 pub trait Plotter {
     fn plot(&self) -> Result<()>;
 }
+
+
+pub enum SvgObject {
+    Line {
+        x1: f32, y1: f32,
+        x2: f32, y2: f32,
+        stroke: Option<String>,
+        stroke_width: f32,
+        hover: Option<String>,
+    },
+
+    Circle {
+        cx: f32, cy: f32, r: f32,
+        fill: String,
+    }
+}
+
+impl SvgObject {
+    fn render(&self) -> String {
+        match self {
+            SvgObject::Line { x1, y1, x2, y2, stroke, stroke_width, hover } => {
+                let mut style = format!("stroke-width: {};", stroke_width);
+                if let Some(stroke) = stroke {
+                    style.push_str(&format!("stroke: {};", stroke));
+                }
+                let mut inner = format!("x1='{}' y1='{}' x2='{}' y2='{}'", x1, y1, x2, y2);
+                if !style.is_empty() {
+                    inner.push_str(&format!(" style='{}'", style));
+                }
+                match hover {
+                    Some(text) => {
+                        format!("<line {}><title>{}</title></line>", inner, text)
+                    }
+                    None => {
+                        format!("<line {}/>", inner)
+                    }
+                }
+            },
+            SvgObject::Circle { cx, cy, r, fill } => {
+                format!("<circle cx='{}' cy='{}' r='{}' fill='{}'/>", cx, cy, r, fill)
+            },
+        }
+    }
+}
+
+pub struct SvgGroup {
+    content: Vec<SvgObject>,
+}
+impl SvgGroup {
+    pub fn new() -> Self {
+        SvgGroup { content: Vec::new() }
+    }
+
+    pub fn push(&mut self, o: SvgObject) {
+        self.content.push(o)
+    }
+
+    pub fn render(&self) -> String {
+        self.content
+            .iter()
+            .map(SvgObject::render)
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+}
